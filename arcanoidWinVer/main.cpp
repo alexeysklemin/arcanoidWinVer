defc83e1cd3a1ae164d62f498a94229e35f71e31
a_skl <a_skl@LAPTOP-ALEX>
2022-04-11 22:01:24 +0400

    def draw function

--- arcanoidWinVer/main.cpp
+++ arcanoidWinVer/main.cpp
@@ -154,7 +154,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
-            // 
+            drowFrame();
             EndPaint(hWnd, &ps);
         }
         break;
